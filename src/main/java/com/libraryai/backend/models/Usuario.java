de4f02686ed051e621efe256fc68f4f3b9dda5ea
keyner <keinerf588@gmail.com>
2026-01-03 14:18:26 -0500

    Archivo modelo de la tabla usuarios, encargada de modelar y representar a la tabla para almacenar los datos de los campos y manipular dichos datos

---
+++ src/main/java/com/libraryai/backend/models/Usuario.java
@@ -0,0 +1,77 @@
+package com.libraryai.backend.models;
+
+import java.time.LocalDate;
+
+//*Clase modelo de la tabla Usuarios
+public class Usuario {
+
+    // **atributos de la tabla y del usuario privados
+    private int usuario_id;
+    private String nombre;
+    private String correo;
+    private String contrasenaHash;
+    private LocalDate fechaRegistro;
+    private boolean activo;
+
+    // **Constructor para crear usuarios
+    public Usuario(int usuario_id, String nombre, String correo, String contrasenaHash, LocalDate fechaRegistro,
+            boolean activo) {
+        this.usuario_id = usuario_id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasenaHash = contrasenaHash;
+        this.fechaRegistro = fechaRegistro;
+        this.activo = activo;
+    }
+
+    // **Getter y Setter para manipulacion de datos privados
+    
+    public int getUsuario_id() {
+        return usuario_id;
+    }
+
+    public void setUsuario_id(int usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenaHash() {
+        return contrasenaHash;
+    }
+
+    public void setContrasenaHash(String contrasenaHash) {
+        this.contrasenaHash = contrasenaHash;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+}
