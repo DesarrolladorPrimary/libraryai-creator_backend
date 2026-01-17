061f06e88f75986565cdce9f24d2cb32cd4d8780
keyner <keinerf588@gmail.com>
2026-01-17 12:54:11 -0500

    Se pasaron las instancias de las claves de entorno a otra clase propia y llamada a donde corresponden

--- src/main/java/com/libraryai/backend/ai/GeminiAI.java
+++ src/main/java/com/libraryai/backend/ai/GeminiAI.java
@@ -5,18 +5,15 @@ import com.google.genai.types.Content;
 import com.google.genai.types.GenerateContentConfig;
 import com.google.genai.types.GenerateContentResponse;
 import com.google.genai.types.Part;
-
-import io.github.cdimascio.dotenv.Dotenv;
+import com.libraryai.backend.config.AIConfig;
 
 public class GeminiAI {
 
-    private static final String API_KEY = Dotenv.load().get("GEMINI_API_KEY");
-
     public static String generarTexto(String responseUser, String instruccionesUser) {
         try (
-            Client client = Client.builder().apiKey(API_KEY).build();
+            Client client = Client.builder().apiKey(AIConfig.API_KEY).build();
         ){
-        
+
             Content system = 
                 Content.builder()
                     .role("system")
@@ -32,7 +29,7 @@ public class GeminiAI {
 
             GenerateContentResponse resp = 
             client.models.generateContent(
-                "gemini-3-flash-preview", 
+                AIConfig.MODEL_AI, 
                 responseUser, 
                 cfg
             );
